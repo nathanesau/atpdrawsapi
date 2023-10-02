df435a3a9a427281a46ca73fb3d5e84e8e446312
Nathan Esau <nathanesau1@gmail.com>
2023-10-02 18:58:42 +0000

    convert tests to pytest

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,13 +1,11 @@
-import unittest
 import os
 from atpdraws import parser
 
-class TestParser(unittest.TestCase):
-    def test_parse_draw(self):
-        draw_filename = os.path.join(os.path.dirname(__file__), "resources/sample_draw.html")
-        with open(draw_filename) as f:
-            data = f.read()
-        draw = parser.parse_draw(data)
+def test_parse_draw():
+    draw_filename = os.path.join(os.path.dirname(__file__), "resources/sample_draw.html")
+    with open(draw_filename) as f:
+        data = f.read()
+    draw = parser.parse_draw(data)
 
-        # current this just contains a list of round1 matchups
-        self.assertEqual(len(draw), 16)
+    # current this just contains a list of round1 matchups
+    assert(len(draw) == 16)
